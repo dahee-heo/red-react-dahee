f517173c918bf9fbc9183fd02fa6e10f986d6733
heo dahee <hdahee.9@gmail.com>
2021-11-07 11:26:56 +0900

    [#2] class to className

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 
-
 function App() {
   return (
     <>
       <Header></Header>
-      <div class="contents">
-        <article class="home">
+      <div className="contents">
+        <article className="home">
           <h1>RED</h1>
           <h2>Welcome to Refrigerator Expiry Date</h2>
         </article>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,24 @@
 function Header() {
   return (
     <header>
-      <div class="logo">
+      <div className="logo">
         RED
       </div>
       <div>
-        <a href="./index.html" class="active" id="menu-a-home"><span class="material-icons-outlined">home</span></a>
+        <a href="./index.html" className="active" id="menu-a-home"><span className="material-icons-outlined">home</span></a>
       </div>
       <div>
-        <a href="./groceries.html" id="menu-a-groceries"><span class="material-icons-outlined">fact_check</span></a>
+        <a href="./groceries.html" id="menu-a-groceries"><span className="material-icons-outlined">fact_check</span></a>
       </div>
-      <div class="menu-items">
-        <a href="./items.html" id="menu-a-items"><span class="material-icons">list</span></a>
+      <div className="menu-items">
+        <a href="./items.html" id="menu-a-items"><span className="material-icons">list</span></a>
         <div><span id="menu-items-counter">1</span></div>
       </div>
-      <div class="empty"></div>
+      <div className="empty"></div>
       <div>
         <a href="#!" id="menu-a-account" onclick="accountToggle(); return false;">
-          <span class="material-icons-outlined">account_circle</span>
-          <ul class="account-menu">
+          <span className="material-icons-outlined">account_circle</span>
+          <ul className="account-menu">
             <li>Guest</li>
             <li>Login</li>
             <li>Hello 홍길동!</li>
