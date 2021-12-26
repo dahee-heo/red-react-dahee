03d205c1194c3427fe8dc8b4baf19168ea02379a
heo dahee <hdahee.9@gmail.com>
2021-12-26 15:23:30 +0900

    [#6] 로그인 후 메뉴 보여주기

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,15 +17,19 @@ function Header(props) {
         {/* <a href="./index.html" className="active" id="menu-a-home"><span className="material-icons-outlined">home</span></a> */}
         <NavLink to="home" activeClassName='active' id="menu-a-home"><span className="material-icons-outlined">home</span></NavLink>
       </div>
-      <div>
-        {/* <a href="./groceries.html" id="menu-a-groceries"><span className="material-icons-outlined">fact_check</span></a> */}
-        <NavLink to="groceries" activeClassName='active' id="menu-a-groceries"><span className="material-icons-outlined">fact_check</span></NavLink>
-      </div>
-      <div className="menu-items">
-        {/* <a href="./items.html" id="menu-a-items"><span className="material-icons">list</span></a> */}
-        <NavLink to="items" activeClassName='active' id="menu-a-items"><span className="material-icons">list</span></NavLink>
-        <div><span id="menu-items-counter">1</span></div>
-      </div>
+      {loginStore.user.uid ? 
+        <>
+          <div>
+            {/* <a href="./groceries.html" id="menu-a-groceries"><span className="material-icons-outlined">fact_check</span></a> */}
+            <NavLink to="groceries" activeClassName='active' id="menu-a-groceries"><span className="material-icons-outlined">fact_check</span></NavLink>
+          </div>
+          <div className="menu-items">
+            {/* <a href="./items.html" id="menu-a-items"><span className="material-icons">list</span></a> */}
+            <NavLink to="items" activeClassName='active' id="menu-a-items"><span className="material-icons">list</span></NavLink>
+            <div><span id="menu-items-counter">1</span></div>
+          </div>
+        </> : <></>
+      }
       <div className="empty"></div>
       <div>
         <a href="#!" id="menu-a-account" onClick={(event) => {event.preventDefault(); setA(!a)}}>
